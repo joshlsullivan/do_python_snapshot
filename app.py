0e648cc9f75098345dac0cd625d82f2a7d3cd490
Josh Sullivan <jlogansullivan@gmail.com>
2015-12-20 20:03:27 -0600

    updated app

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import digitalocean
+import os
 from datetime import date
 today = date.today()
-manager = digitalocean.Manager(token='')
-my_droplets = manager.get_all_droplets()
-for droplet in my_droplets:
-    if droplet.status == 'active':
-        droplet.take_snapshot('{1} {0:%Y%m%d}'.format(today, droplet.name), power_off=True)
+do_token = os.environ['TOKEN']
+manager = digitalocean.Manager(token=do_token)
+
+def main():
+    my_droplets = manager.get_all_droplets()
+    for droplet in my_droplets:
+        if droplet.status == 'active':
+            droplet.take_snapshot('{1} {0:%Y%m%d}'.format(today, droplet.name), power_off=True)
+
+if __name__ == "__main__":
+    main()
